d83f200bbde008b4062d19556f2c6562399fd394
es1024 <es1024@users.noreply.github.com>
2014-11-13 14:57:39 -0800

    updated Drunkards example

--- players/Drunkards/Drunkards.pl
+++ players/Drunkards/Drunkards.pl
@@ -1,10 +1,22 @@
 #!/usr/bin/perl
 use 5.10.1;
-@opts = qw(S R);
-$| = 1;
+$| = 1; # disable buffering
+@actions = qw(S S S S S);
+
+$_ = <>; ~/^INDEX (\d+)/;
+$index = $1;
+
 while(<>){
 	if(index($_, 'START_TURN') == 0){
-		say 'S,S,S,S,S';
+		say join(',', @actions);
+	}elsif(index($_, 'END_DAY') == 0){ # update actions based on who is alive
+		# index 1-indexed; first bot at position 2.
+		my @status = split(',',(split(' '))[$index + 1]);
+		my $i;
+		for($i = 0; $i < 5; ++$i){
+			# action is S if alive, D if dead. Servants will never be in camp.
+			$actions[$i] = $status[$i] eq 'A' ? 'S' : 'D';
+		}
 	}elsif(index($_, 'EXIT') == 0){
 		exit 0;
 	}
